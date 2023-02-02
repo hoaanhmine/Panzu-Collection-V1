8bf7aeae38a5295dabd100d4dded829e0cf3a1f4
HMFNF <51531073+hoaanhmine@users.noreply.github.com>
2023-02-02 13:06:31 +0700

    Add files via upload

--- source/StoryMenuState.hx
+++ source/StoryMenuState.hx
@@ -409,7 +409,7 @@ class StoryMenuState extends MusicBeatState
 		if(assetName == null || assetName.length < 1) {
 			bgSprite.visible = false;
 		} else {
-			bgSprite.loadGraphic(Paths.image('menubackgrounds/menu_' + assetName));
+			bgSprite.loadGraphic(Paths.image('menubackgrounds/' + assetName));
 		}
 		PlayState.storyWeek = curWeek;
 
